8b9ac9cef73e6a2f00c10d1a4a2ab7d95311807a
Султанова Юлия <yuliya.sultanova@mail.ru>
2022-12-08 21:34:11 +0300

    added test registration

--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -1,38 +1,71 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 
-import static com.codeborne.selenide.Condition.text;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class TextBoxTest {
 
     @BeforeAll
     static void beforeAll() {
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
+        open("/automation-practice-form");
+        //WebDriver driver;
+        //driver = getWebDriver();
+        //JavascriptExecutor js = (JavascriptExecutor) driver;
+        //js.executeScript("document.body.style.zoom='80%'");
     }
 
     @Test
     void successFillTest() {
-        open("/text-box");
-        $(".main-header").shouldHave(text("Text Box"));
 
-        $("#userName").setValue("Alexey");
-        $("#userEmail").setValue("alexey@company.com");
-        $("#currentAddress").setValue("Some address 1");
-        $("#permanentAddress").setValue("Other address 2");
-        $("#submit").click();
+        $(".main-header").shouldHave(text("Practice Form"));
 
-        $("#output").shouldHave(text("Alex"), text("alexey@company.com"),
-                text("Some address 1"), text("Other address 2"));
+        $("#firstName").val("Dmitriy");
+        $("#lastName").val("Muromec");
+        $("#userEmail").val("mitya_muromec@zarechye.ru");
+        //$(".gender-radio-1").shouldHave(value("Male")).click();
+        $(byText("Male")).click();
+        $("#userNumber").val("9991891500");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("May");
+        $(".react-datepicker__year-select").selectOptionByValue("1990");
+        $(".react-datepicker__day--017").click();
+        $("#subjectsInput").val("history").pressEnter();
+        $("#subjectsInput").val("biology").pressEnter();
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/resources/lesta.jpg"));
+        $("#currentAddress").val("Zarechye");
+        $("#stateCity-label").scrollTo();
+        $(byText("Select State")).click();
+        $(byText("Haryana")).scrollTo().click();
+        $(byText("Select City")).click();
+        $(byText("Karnal")).click();
+        $("#submit").pressEnter();
 
-        $("#name").shouldHave(text("Alex"));
-        $("#currentAddress").shouldHave(text("Some address 1")); // wrong
-        $("#currentAddress", 1).shouldHave(text("Some address 1"));
-        $("#output").$("#currentAddress").shouldHave(text("Some address 1"));
-        $("#output #currentAddress").shouldHave(text("Some address 1"));
+        $(byText("Thanks for submitting the form")).shouldBe(visible);
+        $(".table-responsive").shouldHave(text("Dmitriy Muromec"));
+        $(".table-responsive").shouldHave(text("mitya_muromec@zarechye.ru"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("9991891500"));
+        $(".table-responsive").shouldHave(text("17 May,1990"));
+        $(".table-responsive").shouldHave(text("History, Biology"));
+        $(".table-responsive").shouldHave(text("Sports, Music"));
+        $(".table-responsive").shouldHave(text("lesta.jpg"));
+        $(".table-responsive").shouldHave(text("Zarechye"));
+        $(".table-responsive").shouldHave(text("Haryana Karnal"));
     }
 }
