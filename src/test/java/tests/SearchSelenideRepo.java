3ee704b8010af307da04ec0ecb5330c3fea85be5
Султанова Юлия <yuliya.sultanova@mail.ru>
2023-03-19 20:16:34 +0300

    commit SearchSelenideRepo.java - commits, for lesson8

--- src/test/java/tests/SearchSelenideRepo.java
+++ src/test/java/tests/SearchSelenideRepo.java
@@ -8,13 +8,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchSelenideRepo {
     @Test
     void shouldFindSelenideInGithub() {
-        // открыть страницу github.com
+        // open github.com
         open("https://github.com/");
-        // ввести в поле поиска selenide и нажать Enter
+        // ввести в поле поиска selenide и press Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         // нажимаем на линк от первого результата поиска
         $$("ul.repo-list li").first().$("a").click();
-        // check: в заголовке встречается selenide/selenide
+        // check: selenide/selenide
         $("h1").shouldHave(text("selenide / selenide"));
     }
 
